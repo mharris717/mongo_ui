3d2a9ac1c61c6d860321b595ae1a7ed6c118aaa7
Mike Harris <mharris717@gmail.com>
2009-12-16 15:24:12 -0500

    current state

--- app.rb
+++ app.rb
@@ -1,32 +1,148 @@
 require 'rubygems'
 require 'sinatra'
 require "/code/mongo_persist/lib/mongo_persist"
+require 'haml'
 
+
+def db
+  Mongo::Connection.new.db('test-db3')
+end
+
+
+class Order
+  include MongoPersist
+  attr_accessor :po_number, :customers, :some_hash
+  mongo_reference_attributes ['customers']
+  fattr(:order_products) { [] }
+  def products
+    order_products.map { |x| x.product }
+  end
+  def subtotal
+    order_products.map { |x| x.subtotal }.sum
+  end
+end
+
+class OrderProduct
+  include MongoPersist
+  attr_accessor :unit_price, :quantity, :product
+  mongo_reference_attributes ['product']
+  def subtotal
+    quantity.to_f * unit_price
+  end
+end
+
+class Product
+  include MongoPersist
+  attr_accessor :name
+end
+
+class Customer
+  include MongoPersist
+  attr_accessor :email
+end
+
+    [Order,Product].each { |x| x.collection.remove }
+    @products = [Product.new(:name => 'Leather Couch'),Product.new(:name => 'Maroon Chair')].each { |x| x.mongo.save! }
+    @customers = [Customer.new(:email => 'a'),Customer.new(:email => 'b')].each { |x| x.mongo.save! }
+
+    @orders = []
+    @orders << Order.new(:customers => @customers, :po_number => 1234, :order_products => [OrderProduct.new(:unit_price => 1000, :quantity => 1, :product => @products[0])]).mongo.save!
+    @orders << Order.new(:customers => @customers, :po_number => 1235, :order_products => [OrderProduct.new(:unit_price => 200, :quantity => 2, :product => @products[1])]).mongo.save!
+
+  
 class Mongo::Collection
   def keys
     find.map { |x| x.keys }.flatten.uniq.sort.reject { |x| x.to_s[0..0] == '_' }
   end
 end
 
-def db
-  Mongo::Connection.new.db('test-db')
+
+
+class Foo
+  class << self
+    fattr(:colls) do
+      ['c1','c2'].map do |n|
+        coll = db.collection(n)
+        coll.remove
+        coll.save('name' => 'Mike')
+        coll.save('name' => 'Dave', 'age' => 25)
+        coll.save('name' => 'Lowell', 'age' => 39, 'schools' => ['Williams','Columbia','George Mason'])
+        coll
+      end
+    end
+  end
 end
 
-coll = db.collection("test")
-coll.remove
-coll.save('a' => 'b')
-coll.save('c' => 'd', 'a' => 7)
+Foo.colls
 
 get "/" do
-  @coll = coll
+  @colls = db.collections.reject { |x| x.name == 'system.indexes' }
   haml :coll
 end
 
+def mongo_value(v)
+  if v[0..0] == '['
+    v[1...-1].split(",").map { |x| x.tmo }
+  else
+    v.tmo
+  end
+end
+
+class Object
+  def mongo_inspect
+    "#{self}"
+  end
+  def tmo
+    self
+  end
+end
+
+class Numeric
+  def tmo
+    (self == to_i) ? to_i : self
+  end
+end
+
+class String
+  def num?
+    self =~ /^[\d\.]*$/
+  end
+  def tmo
+    num? ? to_f.tmo : self
+  end
+end
+
+class Array
+  def mongo_inspect
+    "<ul>" + map { |x| "<li>#{x.mongo_inspect}</li>" }.join("") + "</ul>"
+  end
+end
+
+class Hash
+  def mongo_inspect
+    "<ul>" + map { |k,v| "<li>#{k.mongo_inspect}: #{v.mongo_inspect}</li>" }.join("") + "</ul>"
+  end
+end
+
+class Hash
+  def without_keys(*ks)
+    ks = ks.flatten
+    res = {}
+    each do |k,v|
+      res[k] = v unless ks.include?(k)
+    end
+    res
+  end
+end
+
 get '/new_row' do
-  coll.save(params)
+  puts params.inspect
+  coll = db.collection(params[:coll])
+  new_params = params.without_keys('coll','newField').map_value { |v| mongo_value(v) }
+  coll.save(new_params)
 end
 
 get '/table' do
-  @coll = coll
-  haml :table
+  coll = db.collection(params[:coll])
+  haml :table, :locals => {:coll => coll}
 end
--- public/run.js
+++ public/run.js
@@ -1,38 +1,68 @@
-function newRow() {
-    var fields = $('table tr:first th')
-    var str = "<form id='form' action='/new_row'>"
-    str += "<tr class='new-row'>"
-    
-    for(var i=0;i<fields.length;i++) {
-        str += "<td><input type='text' name='" + fields[i].innerText + "'></td>"
+function tag(t,cont) {
+    var str = "<" + t + " "
+    str += ">" + cont + "</" + t + ">"
+    return str
+}
+
+function textInput(n) {
+    return "<input type='text' name='" + n + "' >"
+}
+
+function coll(n) {
+    var collName = n
+    function collScope(str) {
+        return $('#' + collName + " " + str)
     }
-    str += "<td><input type='submit' value='abc'></td>"
-    str += "</tr>"
-    str += "</form>"
+    function headerVal(cell) {
+        if ($('input',cell).length > 0) {
+            return $('input',cell).val()
+        }
+        else {
+            return $(cell).text()
+        }
+    }
     
-    $('table').append(str)
-    $('#form').ajaxForm({success: reloadTable})
+    function newRow() {
+        var fields = collScope('table tr:first th')
+        var hid = "<input type='hidden' name='coll' value='" + collName + "'>"
+        var str = "<tr class='new-row'>"
+    
+        for(var i=0;i<fields.length;i++) {
+            str += tag("td",textInput(headerVal(fields[i])))
+        }
+        str += "<td>" + hid + "<input type='submit' value='Save'></td></form>"
+        str += "</tr>"
+        //collScope('table').before("<form action='/new_row'>")
+        //collScope('table').after('dd')
+        collScope('table').append(str)
+        collScope('form').submit(function() { 
+            //setNewField()
+            $(this).ajaxSubmit({success: reloadTable}); 
+            return false; 
+        });
+    }
+    
+    function setNewField() {
+        //formData['abc'] = 'xyz'
+        // var n = collScope("table tr:first input").val()
+        // alert("setting name attr to " + n)
+        // collScope("tr input").attr('name',n)
+    }
+
+    function reloadTable() {
+        $.get('/table',{coll: collName},function(data) {
+            collScope('').html(data)
+        })
+    }
+    function newColumn() {
+        collScope('table tr:first').append(tag("th",textInput('newField')))
+    }
+    this.setupNewRow = function() {
+        collScope('a.new-row').live('click',newRow)
+        collScope('a.new-column').live('click',newColumn)
+    }
+    return this;
 }
 
-function saveNewRow() {
-    var h = {}
-    $('tr.new-row td input').each(function() {
-        h[$(this).attr('name')] = $(this).val()
-    })
-    $.post("/new_row",h)
-}
-
-function reloadTable() {
-  $.get('/table',function(data) {
-    $('.collection').html(data)
-  })
-}
-
-$(function() {
-    $('a.new-row').click(function() {
-        newRow()
-    })
-    // $('tr.new-row a.save').live('click',function() {
-    //     saveNewRow()
-    // })
-})
+$(coll('c1').setupNewRow)
+$(coll('c2').setupNewRow)
