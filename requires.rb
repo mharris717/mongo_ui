f2175fd8505bc4fa38d644d69c51f440a324f52e
Mike Harris <mharris717@gmail.com>
2010-04-15 14:19:16 -0400

    requires

--- requires.rb
+++ requires.rb
@@ -1,12 +1,12 @@
 # require 'rubygems'
-if ARGV.include?('--localmp') || true
+if ARGV.include?('--localmp') 
   require "/code/mongo_persist/lib/mongo_persist"
 else
   require 'mongo_persist'
 end
 # require 'haml'
-#require 'mongo_scope'
-require "/code/mongo_scope/lib/mongo_scope"
+require 'mongo_scope'
+#require "/code/mongo_scope/lib/mongo_scope"
 # require 'activesupport'
 require 'facets/file/write'
 
